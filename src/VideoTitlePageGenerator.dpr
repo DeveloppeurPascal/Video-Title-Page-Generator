bb19becbafed15e007cdcf6174d82a916394ffa1
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-21 10:58:56 +0200

    added copyright and timestamp comments

--- src/VideoTitlePageGenerator.dpr
+++ src/VideoTitlePageGenerator.dpr
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Video Title Page Generator
+
+  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://videotitlepagegenerator.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Video-Title-Page-Generator
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:29.661+02:00
+  Signature : a5062ea3c2356434da5b01b39ef89fff2c4715e5
+  ***************************************************************************
+*)
+
 program VideoTitlePageGenerator;
 
 uses
--- src/fMain.pas
+++ src/fMain.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  Video Title Page Generator
+
+  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://videotitlepagegenerator.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Video-Title-Page-Generator
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:29.661+02:00
+  Signature : 060ed373ec5eecc3dea1c4f89f8c7f5639c2522e
+  ***************************************************************************
+*)
+
 unit fMain;
 
 interface
